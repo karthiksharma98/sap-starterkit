6b4c72676d9c66906b6478229d2680131cc9c847
Kartikeya Sharma <kartikes@trinity.vision.cs.cmu.edu>
2021-04-11 15:47:43 -0400

    bugfix

--- start_det_baseline.sh
+++ start_det_baseline.sh
@@ -5,7 +5,7 @@ scale=1.0
 factor=1.0
 
 python det_baseline_perf.py \
-    --config "$HOME/repo/mmdetection/configs/mask_rcnn/mask_rcnn_r50_fpn_2x_coco.py" \
+    --config "$HOME/mmdetection/configs/mask_rcnn/mask_rcnn_r50_fpn_2x_coco.py" \
 	--weights "$dataDir/ModelZoo/mmdet/mask_rcnn_r50_fpn_2x_coco_bbox_mAP-0.392__segm_mAP-0.354_20200505_003907-3e542a40.pth" \
     --annot-path "$dataDir/Argoverse-HD/annotations/val.json" \
     --eval-config "./config.json" \
